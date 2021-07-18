e04df1eab9c89f7f670cd1a7d8a21d07b72e9a1d
rheapradhan21 <87064604+rheapradhan21@users.noreply.github.com>
2021-07-18 11:55:04 +0300

    Add files via upload

---
+++ Project 12.js
@@ -0,0 +1,19 @@
+var sea = createSprite();
+function preload(){
+
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadImage("ship-1.png");
+
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  draw;
+  background("blue");
+ background.velocityX = 5;
+}
